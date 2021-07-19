c550007783b4a694b605206f38953f7ffce96784
isaacW <ISAAC.WILSON1811@STU.MTEC.EDU>
2021-07-19 16:34:06 -0600

    unit 5 complete

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { DepartmentsService } from '../../services/departments.service';
+import { Department } from '../../interfaces/department';
 
 @Component({
   selector: 'app-departments',
   templateUrl: './departments.component.html',
-  styleUrls: ['./departments.component.scss']
+  styleUrls: ['./departments.component.scss'],
 })
-export class DepartmentsComponent implements OnInit {
 
-  constructor() { }
+export class DepartmentsComponent implements OnInit {
+  @Injectable() departments: Department[] = [];
+
+  constructor(
+    private departmentsService: DepartmentsService
+  ) { }
 
   ngOnInit(): void {
+    this.departments = this.departmentsService.departments;
   }
-
 }
